f62c54b650175cb8d636414260252200ee03e472
Ryan Gourley <rgourley@cra.com>
2014-06-13 11:11:47 -0400

    end of tutorial step 2

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,5 +1,20 @@
-    'use strict';
+'use strict';
      
-    app.controller('PostsCtrl', function ($scope) {
-     
-    });
+app.controller('PostsCtrl', function ($scope) {
+	//holds all of the submitted posts
+	$scope.posts = [];
+	//format for the information stored by each post
+	$scope.post = {url: 'http://', title: ''};
+	//method to put a newly submitted post into posts, and reset form
+	$scope.submitPost = function(){
+		$scope.posts.push($scope.post);
+		$scope.post = {url: 'http://', title: ''};
+	};
+
+	//deletes the given post 'index' from the array
+	$scope.deletePost = function (index){
+		$scope.posts.splice(index, 1);
+	};
+ 
+
+});
