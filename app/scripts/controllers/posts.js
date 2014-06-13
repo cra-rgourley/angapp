6cf0ddc3d50a5003395a4b3531a5cf76ebbd4119
Ryan Gourley <rgourley@cra.com>
2014-06-13 15:34:02 -0400

    STEP 5 FINAL

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -8,12 +8,13 @@ app.controller('PostsCtrl', function ($scope, $location, Post) {
 	//format for the information stored by each post
 	$scope.post = {url: 'http://', title: ''};
 
+	/* NO LONGER IN THIS SCOPE, MOVED TO NAV BAR
 	//method to put a newly submitted post into posts, and reset form
 	$scope.submitPost = function(){
 		Post.create($scope.post).then(function(ref){
 			$location.path('/posts/'+ref.name());
 		});
-	};
+	};*/
 
 	//deletes the given post 'index' from the array
 	$scope.deletePost = function (postId){
