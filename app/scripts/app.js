3e5f736e864591532c52d0104fb5d08197a52817
Ryan Gourley <rgourley@cra.com>
2014-06-16 09:28:31 -0400

    END OF STEP 6, users with authentication working.  Register also working.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,6 +31,14 @@ app.config(function ($routeProvider) {
         templateUrl: 'views/posts.html',
         controller: 'PostsCtrl'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl'
+      })
       .when('/posts/:postId',{
         templateUrl: 'views/showpost.html',
         controller: 'PostViewCtrl'
